f4d4071e6b79ef8df64f5c83d8e80e467b70903d
WoodenAndroid <jmg8766@rit.edu>
2019-03-28 09:15:44 -0400

    adding fancy styles to icon links.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,45 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 import LinkedIn from "../images/linkedin-square.svg"
+import LinkedInHighlighted from "../images/linkedin-square-alizarin.svg"
 import GitHub from "../images/github-square.svg"
+import GitHubHighlighted from "../images/github-square-alizarin.svg"
 import Email from "../images/email.svg"
+import EmailHighlighted from "../images/email-alizarin.svg"
+
+const IconLink = ({ link, image, hoverImage, alt }) =>
+    <a href={link} target="_blank" rel="noopener noreferrer">
+      <img src={image}
+           width="27px"
+           height="27px"
+           alt={alt}
+           onMouseOver={e => {
+             e.currentTarget.src = hoverImage
+             e.currentTarget.width = 32
+             e.currentTarget.height = 32
+           }}
+           onMouseOut={e => {
+             e.currentTarget.src = image
+             e.currentTarget.width = 27
+             e.currentTarget.height = 27
+           }}
+      />
+    </a>
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <div className="centered">
       <h1>Justin Gottshall</h1>
-      <p>
-        Freelance Full-Stack Developer
-        <a href="https://www.linkedin.com/in/justin-gottshall-25606b107" target="_blank">
-          <img src={LinkedIn} width="27.033px" height="27.033px" alt="LinkedIn icon"/>
-        </a>
-        <a href="https://github.com/jmg8766" target="_blank">
-          <img src={GitHub} width="27.033px" height="27.033px" alt="GitHub icon"/>
-        </a>
-        <a href="mailto:justin.gottshall@gmail.com">
-          <img src={Email} width="27.033px" height="27.033px" alt="email icon"/>
-        </a>
-      </p>
+      <p>Freelance Full-Stack Developer</p>
+      <div>
+        <IconLink link="https://www.linkedin.com/in/justin-gottshall-25606b107" image={LinkedIn} hoverImage={LinkedInHighlighted} alt="LinkedIn Icon"/>
+        <IconLink link="https://github.com/jmg8766" image={GitHub} hoverImage={GitHubHighlighted} alt="Github Icon"/>
+        <IconLink link="mailto:justin.gottshall@gmail.com" image={Email} hoverImage={EmailHighlighted} alt="Email Icon"/>
+      </div>
     </div>
   </Layout>
 )
