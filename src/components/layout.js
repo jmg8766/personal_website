5c16695c56398711858ad5681510f911ccfe75d1
WoodenAndroid <jmg8766@rit.edu>
2019-04-22 11:08:18 -0400

    put my name in the title meta tag to improve SEO

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,7 +4,7 @@ import SEO from "../components/seo"
 export default ({ children }) =>
   <main>
     <SEO 
-      title="Home" 
+      title="Justin Gottshall | Full-Stack Developer" 
       description="Full-Stack Developer living in Cleveland, OH. Currently accepting new clients!" 
       keywords={[`Justin Gottshall`, `Personal Website`, `Software Engineer`, `Full-Stack`, `Developer`, `Cleveland`]} 
     />
